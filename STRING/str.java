c3ba1e2e74a04b7bdd1cd9ad0eb79b4e1abce6b9
Nandhu-g21 <145680608+Nandhu-g21@users.noreply.github.com>
2025-03-06 10:23:32 +0530

    Add files via upload

---
+++ STRING/str.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class str{
+    public static void main(String[]args){
+        String s="VENKAT";
+        System.out.println(s);
+        System.out.println(s.length());
+        System.out.println(s.charAt(4));
+    }
+}
